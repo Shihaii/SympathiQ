b8c82aa7ea88bed887084d09ef74d4f27b1bc54b
Shihai <Kirill2004val@gmail.com>
2024-08-22 16:22:00 +0300

    fix(auth): deleted unused import

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import './assets/tailwind.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import VueTheMask from 'vue-the-mask'
 
 import App from './App.vue'
 import router from './router'
@@ -12,5 +11,4 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-app.use(VueTheMask)
 app.mount('#app')
