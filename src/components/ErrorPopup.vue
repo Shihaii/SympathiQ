b0302ce7cdc28ee8085b87849ec852f879abc4f4
Shihai <Kirill2004val@gmail.com>
2024-09-02 10:56:10 +0300

    feat(auth): added error popup on submit

---
+++ src/components/ErrorPopup.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const props = defineProps<{
+  errorTitle: string
+  errorText: string
+}>()
+</script>
+
+<template>
+  <div
+    class="fixed top-36 right-16 flex flex-col gap-[0.8rem] z-[1] max-w-[32.6rem] w-full rounded-2xl px-[2.4rem] py-[1.6rem] bg-[#f41a0c0d] shadow-md"
+  >
+    <h3 class="font-['Jost'] font-semibold text-[1.4rem] leading-[1.8rem] text-[#F41A0C]">
+      {{ errorTitle }}
+    </h3>
+    <span class="font-['Jost'] font-normal text-[1.2rem] leading-[1.6rem] text-[#F41A0C]">
+      {{ errorText }}</span
+    >
+  </div>
+</template>
--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,8 +1,9 @@
 import router from '@/router'
 import { useUserStore } from '@/stores/user'
 import axios from 'axios'
+import type { Ref } from 'vue'
 
-export const createUser = (values: any) => {
+export const createUser = (values: any, errors: Ref<string>) => {
   delete values.agree
   axios
     .post(`https://api-scipro.sheep.fish/wp-json/api/v1/registration`, {
@@ -10,25 +11,30 @@ export const createUser = (values: any) => {
     })
     .then((response: { data: any }) => {
       console.log(response.data)
+      const user = useUserStore()
+      user.login(response.data)
+    })  
+    .catch((error) => {
+      errors.value = String(error.response.data.message)
     })
 }
 
-export const loginUser = (values: any) => {
-  
-  axios
+export const loginUser = async (values: any, errors: Ref<string>) => {
+  await axios
     .post(`https://api-scipro.sheep.fish/wp-json/api/v1/authorization`, {
       ...values
     })
     .then((response: { data: any }) => {
-      console.log(response.data)
-      if (response.status == 200){
-        const user = useUserStore();
-        user.login(response.data);
-      }
+      console.log('response.data - ', response.data)
+      const user = useUserStore()
+      user.login(response.data)
+    })
+    .catch((error) => {
+      errors.value = String(error.response.data.message)
     })
 }
 
-export const forgotPassword = (values: any) => {
+export const forgotPassword = (values: any, errors: Ref<string>) => {
   axios
     .post(`https://api-scipro.sheep.fish/wp-json/api/v1/forgot_password`, {
       ...values
@@ -37,9 +43,12 @@ export const forgotPassword = (values: any) => {
       console.log(response.data)
       if (response.status == 200) router.push('/forgot-password/success')
     })
+    .catch((error) => {
+      errors.value = String(error.response.data.message)
+    })
 }
 
-export const resetPassword = (values: any) => {
+export const resetPassword = (values: any, errors: Ref<string>) => {
   delete values.confirm_password
   axios
     .post(`https://api-scipro.sheep.fish/wp-json/api/v1/reset_password`, {
@@ -48,5 +57,8 @@ export const resetPassword = (values: any) => {
     .then((response: { data: any }) => {
       console.log(response.data)
       if (response.status == 200) router.push('/restore-password/success')
+    })
+    .catch((error) => {
+      errors.value = String(error.response.data.message)
     })
 }
