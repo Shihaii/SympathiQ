58dd83f9d47abb1dde39fb5997a0c24c73cf3245
Shihai <Kirill2004val@gmail.com>
2024-09-02 17:32:12 +0300

    feat(header): completed header visual

--- src/components/Home/Header.vue
+++ src/components/Home/Header.vue
@@ -4,28 +4,36 @@ import MiniProfile from '../Home/MiniProfile.vue'
 </script>
 
 <template>
-  <header class="flex h-[8.6rem] w-full px-[2.9rem] items-center">
+  <header class="flex items-center h-[8.6rem] w-full px-[2.9rem]">
     <Search></Search>
     <button
-      class="ml-[2.5rem] h-16 flex items-center gap-[0.8rem] rounded-full px-[2.8rem] bg-gradient-to-r from-[#1A47BF] to-[#D63ACA] text-white text-[1.4rem] leading-8 font-semibold font-[Jost]"
+      class="flex items-center gap-[0.8rem] ml-[2.5rem] h-16 px-[2.8rem] bg-gradient-to-r from-[#1A47BF] to-[#D63ACA] text-[1.4rem] leading-8 font-['Jost'] font-semibold text-white"
     >
       <img src="/src/assets/images/lightning-icon.svg" />
       Upgrade Your Profile
     </button>
 
     <button
-      class="ml-[1.4rem] w-[4.2rem] h-[4.2rem] flex justify-center items-center rounded-full border-[0.1rem]"
+      class="flex justify-center items-center ml-[1.4rem] border-[0.1rem] rounded-full w-[4.2rem] h-[4.2rem]"
     >
       <img src="/src/assets/images/bell-notification-icon.svg" />
     </button>
 
     <button
-      class="ml-[1.4rem] w-[4.2rem] h-[4.2rem] flex justify-center items-center rounded-full border-[0.1rem]"
+      class="relative flex justify-center items-center ml-[1.4rem] border-[0.1rem] rounded-full w-[4.2rem] h-[4.2rem]"
     >
       <img src="/src/assets/images/bag-notification-icon.svg" />
+      <div
+        class="absolute top-[-0.4rem] right-[-0.4rem] flex items-center justify-center rounded-full max-w-[2.1rem] w-full max-h-[2.1rem] h-full bg-white"
+      >
+        <div
+          class="rounded-full max-w-[1.7rem] w-full max-h-[1.7rem] h-full bg-gradient-to-r from-[#1A47BF] to-[#D63ACA]"
+        >
+          <span class="text-[1rem] leading-[1.4rem] font-['Jost'] font-medium text-white">2</span>
+        </div>
+      </div>
     </button>
 
     <MiniProfile></MiniProfile>
-
   </header>
 </template>
