5b506a3293208db80170613a116d8862782f1c4a
Shihai <Kirill2004val@gmail.com>
2024-09-02 17:30:56 +0300

    fix(auth): Error popup changed order of css classes

--- src/components/ErrorPopup.vue
+++ src/components/ErrorPopup.vue
@@ -7,12 +7,12 @@ const props = defineProps<{
 
 <template>
   <div
-    class="fixed top-36 right-16 flex flex-col gap-[0.8rem] z-[1] max-w-[32.6rem] w-full rounded-2xl px-[2.4rem] py-[1.6rem] bg-[#f41a0c0d] shadow-md"
+    class="fixed z-[1] top-36 right-16 flex flex-col gap-[0.8rem] shadow-md rounded-2xl max-w-[32.6rem] w-full px-[2.4rem] py-[1.6rem] bg-[#f41a0c0d]"
   >
-    <h3 class="font-['Jost'] font-semibold text-[1.4rem] leading-[1.8rem] text-[#F41A0C]">
+    <h3 class="text-[1.4rem] leading-[1.8rem] font-['Jost'] font-semibold text-[#F41A0C]">
       {{ errorTitle }}
     </h3>
-    <span class="font-['Jost'] font-normal text-[1.2rem] leading-[1.6rem] text-[#F41A0C]">
+    <span class="text-[1.2rem] leading-[1.6rem] font-['Jost'] font-normal text-[#F41A0C]">
       {{ errorText }}</span
     >
   </div>
