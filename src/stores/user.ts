e727532fec64754e0ed3cb9719502d9325d6d98b
Shihaii <kirill2004val@gmail.com>
2025-03-07 17:07:59 +0200

    fix(userStore): refactored

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -7,20 +7,18 @@ const baseUrl = `${import.meta.env.VITE_API_URL}/users`
 export const useUserStore = defineStore({
   id: 'auth',
   state: () => ({
-    // initialize state from local storage to enable user to stay logged in
     user: Cookies.get('user') ?? null
   }),
-  // localStorage.getItem('user')
-  // JSON.parse(this.$cookies.get('user'))
+
   actions: {
-    async login(user) {
+    async login(user : string | null) {
       this.user = user
       Cookies.set('user', JSON.stringify(user), { expires: 30 })
       router.push('/')
     },
     logout() {
       this.user = null
-      localStorage.removeItem('user')
+      Cookies.remove('user')
       router.push('/login')
     }
   }
