8491deb6812dc8c73c0d75c95735ea4f8c0e5068
Shihaii <kirill2004val@gmail.com>
2025-03-07 17:09:07 +0200

    fix(home): fixed styles, added suspense

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,12 +4,14 @@ import SideBar from '../components/Home/SideBar.vue'
 </script>
 
 <template>
-  <main class="flex">
+  <main class="flex max-w-[144rem] mx-auto">
     <SideBar></SideBar>
     <div class="flex flex-col w-full">
       <Header></Header>
       <section class="w-full pt-8 pb-24 px-16">
-        <RouterView></RouterView>
+        <Suspense>
+          <RouterView></RouterView>
+        </Suspense>
       </section>
     </div>
   </main>
